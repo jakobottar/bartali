1978ace01188a3ad770612ec87226ac3b8d877ff
jakobottar <jakobottar@pm.me>
2023-04-25 10:50:34 -0600

    add command line overrule of config file

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,11 @@ from .data import MagImageDataset, OODDataset
 from .ddp import Clamp, cleanup, get_color_distortion, prepare_dataloaders, setup
 from .lars_optimizer import LARS
 from .losses import NTXent
-from .utils import RunningAverage, entropy, parse_config_file, roll_objects, variance
+from .utils import (
+    RunningAverage,
+    entropy,
+    parse_config_file,
+    parse_config_file_and_overrule,
+    roll_objects,
+    variance,
+)
